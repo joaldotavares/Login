c3c9da247dea3ecb7989b8812e013743ce495d47
joaldotavares <joaldo.junior@ucsal.edu.br>
2021-09-16 11:12:51 -0300

    refatoração das activitys, nomes dos arquivos drawable e manifest

--- app/src/main/java/br/com/joaldo/login/DashboardActivity.kt
+++ app/src/main/java/br/com/joaldo/login/DashboardActivity.kt
@@ -1,24 +1,22 @@
 package br.com.joaldo.login
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import br.com.joaldo.login.user.User
+import br.com.joaldo.login.R.id
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         title = "Dashboard"
-        var username = findViewById<TextView>(R.id.dashboard_username)
-        var password = findViewById<TextView>(R.id.dashboard_password)
+        val username = findViewById<TextView>(id.dashboard_username)
+        val password = findViewById<TextView>(id.dashboard_password)
 
-        var buser: Bundle = intent.extras!!
+        val buser: Bundle = intent.extras!!
 
-        username.setText(buser.getString("username"))
-        password.setText(buser.getString("password"))
+        username.text = buser.getString("username")
+        password.text = buser.getString("password")
 
     }
 }
